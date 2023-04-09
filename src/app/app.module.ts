a71f335a07e464f39bea90c3b33875aeeeff53e1
HalaszPaul <115172462+HalaszPaul@users.noreply.github.com>
2023-04-09 13:49:59 +0300

    added some changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { WeatherComponent } from './weather/weather.component';
 import {RouterModule} from '@angular/router';
 import {allAppRoutes} from './routes';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ApixuService } from "./apixu.service";
 
 @NgModule({
   declarations: [
@@ -14,9 +17,11 @@ import {allAppRoutes} from './routes';
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(allAppRoutes)
+    RouterModule.forRoot(allAppRoutes),
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ApixuService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
