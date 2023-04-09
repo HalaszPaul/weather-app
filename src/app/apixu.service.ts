11098184adf752e00d91c41779cb9448094b8d81
HalaszPaul <115172462+HalaszPaul@users.noreply.github.com>
2023-04-09 17:58:00 +0300

    "changed some files and fixed some bugs"

--- src/app/apixu.service.ts
+++ src/app/apixu.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApixuService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getWeather(location: string){
+    return this.http.get(
+        'https://api.apixu.com/v1/current.json?key=67755f14ff49e8cce8d732da29319af5/' + location
+    );
+  }
 }
